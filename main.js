f83a6593dad2986e1bc6fc96f6b834270f521c56
jeongjulee <ljj3342@naver.com>
2022-11-28 14:16:56 +0900

    Feat: add observer

--- main.js
+++ main.js
@@ -96,6 +96,14 @@ const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonials', '#co
 const navItems = sectionIds.map(id=> document.querySelector(`[data-link="${id}"]`))
 const sections = sectionIds.map(id => document.querySelector(id));
 
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+    selectedNavItem.classList.remove('active');
+    selectedNavItem = selected;
+    selectedNavItem.classList.add('active');
+}
+
 const observerOptions = {
     root: null,
     rootMargin: '0px',
@@ -103,7 +111,15 @@ const observerOptions = {
 };
 const observerCallback = (entries, observer) => {
     entries.forEach(entry => {
-
+        if(!entry.isIntersecting && entry.intersectionRatio > 0){
+            const index = sectionIds.indexOf(`#${entry.target.id}`);
+        
+            if(entry.boundingClientRect.y < 0) {
+                selectedIndex = index + 1;
+            } else {
+                selectedIndex = index - 1;
+            }
+        } 
     });
 };
 const observer = new IntersectionObserver(observerCallback, observerOptions);
